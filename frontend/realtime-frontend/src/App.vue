e98cd240cc963ecb66342084063e58fd142938f4
Mael Pilon-Joly <mael.pilon-joly@hotmail.com>
2024-05-04 11:25:01 -0400

    remove sub git folder frontend

---
+++ frontend/realtime-frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ users }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: "",
+    };
+
+  },
+  mounted() {
+
+     fetch("/users/")
+      .then((response) => response.text())
+      .then((data) => {
+        this.users = data;
+      });
+  },
+};
+
+</script>
